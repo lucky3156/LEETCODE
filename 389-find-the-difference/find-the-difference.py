d0ad9725a665e164176eeb000f06cf8d991f0fd7
lucky3156 <142299071+lucky3156@users.noreply.github.com>
2025-06-17 00:11:57 +0530

    Time: 3 ms (54.58%) | Memory: 18 MB (23.81%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,8 +1,9 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) :
-        se = ''.join(sorted(s))
-        te = ''.join(sorted(t))
-        for i in range(0,len(se)):
-            if se[i]!=te[i]:
-                return te[i]
-        return te[-1]
+        xor = 0 
+        for char in s+t  :
+            xor^=ord(char)
+        return chr(xor)
+
+            
+        
