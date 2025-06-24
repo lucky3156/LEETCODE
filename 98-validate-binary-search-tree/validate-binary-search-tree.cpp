ccf1e0b539afa09c697d98f4854d4aeab19a5dc6
lucky3156 <142299071+lucky3156@users.noreply.github.com>
2025-06-24 16:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (94.75%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,38 +11,30 @@
  */
 class Solution {
 public:
-    // bool validate(TreeNode* root,int mini,int maxi){
-    //     if (!node) return true;
-    //     if  (root->val <= mini || root->val >=maxi) return false
-    // }
-    vector<int> inord(TreeNode* root,vector<int> &sol){
-        if(root==nullptr) return sol;
-        inord(root->left,sol);
-        sol.push_back(root->val);
-        inord(root->right,sol);
-        return sol;
+    bool validate(TreeNode* root,long mini,long maxi){
+        if (!root) return true;
+        if  (root->val <= mini || root->val >=maxi) return false;
+        return validate(root->left,mini,root->val) && validate(root->right,root->val,maxi);
+        //return validate(root->left, mini, root->val) && validate(root->right, root->val, maxi);
     }
+    // vector<int> inord(TreeNode* root,vector<int> &sol){
+    //     if(root==nullptr) return sol;
+    //     inord(root->left,sol);
+    //     sol.push_back(root->val);
+    //     inord(root->right,sol);
+    //     return sol;
+    // }
     bool isValidBST(TreeNode* root) {
-        
-        // if(root->left->left){
-        //     isValidBST(root->left);
-        // }
-        // if(root->right->right){
-        //     isValidBST(root->right);
-        // }
-        // if(root->val < root->left->val || root->val > root->right->val){
-        //     return false;
+        // vector<int> sol;
+        // sol=inord(root,sol);
+        // for(int i=1;i<sol.size();i++){
+        //    // cout<<sol[i]<<" ";
+        //     if(sol[i]<=sol[i-1]){
+        //         return false;
+        //     }
         // }
         // return true;
-        vector<int> sol;
-        sol=inord(root,sol);
-        for(int i=1;i<sol.size();i++){
-           // cout<<sol[i]<<" ";
-            if(sol[i]<=sol[i-1]){
-                return false;
-            }
-        }
-        return true;
+        return validate(root,LONG_MIN,LONG_MAX);
 
     }
 };
