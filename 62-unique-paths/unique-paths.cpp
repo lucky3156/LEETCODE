31204f5af94b931427b04699cf1e1df710214d7a
lucky3156 <142299071+lucky3156@users.noreply.github.com>
2025-06-26 20:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (8.86%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    // int upath(int m,int n,vector<vector<int>> &dp){
-    //     if(m<0 || n<0){
-    //         return 0;
-    //     }
-    //     if(m==0 && n==0){
-    //         return 1;
-    //     }
-    //     if(dp[m][n]!=-1){
-    //         return dp[m][n];
-    //     }
-    //     return upath(m-1,n,dp)+upath(m,n-1,dp);
-    // }
-    int uniquePaths(int m, int n) {
-        // vector<vector<int>> dp(m,vector<int>(n,-1));
-        // return upath(m-1,n-1,dp);
-        vector<vector<int>> dp(m,vector<int>(n,0));
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(j==0 || i==0){
-                    dp[i][j]=1;
-                }
-                else{
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                }
-            }
+    int upath(int m,int n,vector<vector<int>> &dp){
+        if(m<0 || n<0){
+            return 0;
         }
-        return dp[m-1][n-1];
+        if(m==0 && n==0){
+            return 1;
+        }
+        if(dp[m][n]!=-1){
+            return dp[m][n];
+        }
+        return dp[m][n]=upath(m-1,n,dp)+upath(m,n-1,dp);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return upath(m-1,n-1,dp);
+        // vector<vector<int>> dp(m,vector<int>(n,0));
+
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(j==0 || i==0){
+        //             dp[i][j]=1;
+        //         }
+        //         else{
+        //             dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
     }
 };
