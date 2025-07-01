b9d49605c35e5f79e2620acc84d3757d5ee22e7f
lucky3156 <142299071+lucky3156@users.noreply.github.com>
2025-07-01 18:17:34 +0530

    Time: 112 ms (5.07%) | Memory: 17.3 MB (26.85%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        int v=dist[0].size();
+        for(int k=0;k<v;k++){
+		    for(int i=0;i<v;i++){
+			    for(int j=0;j<v;j++){
+				    dist[i][j]=min(
+				                    dist[i][j] , dist[i][k]+dist[k][j]
+				                   );
+			    }
+		    }
+	    }
+    }
+    vector<vector<int>> createadj(vector<vector<int>>& edge,int v){
+        vector<vector<int>> dist(v,vector<int>(v,1e8));
+        for(auto vi:edge){
+            int i=vi[0];
+            int j=vi[1];
+            int d=vi[2];
+            dist[i][j]=d;
+            dist[j][i]=d;
+        }
+        for(int i=0;i<v;i++){
+			    for(int j=0;j<v;j++){
+				    if(i==j){
+                        dist[i][j]=0;
+                    }
+			    }
+		    }
+        return dist;
+    }
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist=createadj(edges,n);
+        floydWarshall(dist);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<<dist[i][j]<<"  ";
+            }
+            cout<<endl;
+        }
+        vector<int> rows;
+        int count=0;
+        for(auto vect:dist){
+            for(int i=0;i<n;i++){
+                if(vect[i]!=0 && vect[i]<=distanceThreshold){
+                    count++;
+                }
+            }
+            rows.push_back(count);
+            count=0;
+        }
+        for(int i=0;i<n;i++){
+            cout<<rows[i]<<" ";
+        }
+        int mini=rows[0];
+        int ind=0;
+        for(int i=0;i<n;i++){
+            if(rows[i]<=mini){
+                mini=rows[i];
+                ind=i;
+            }
+        }
+        return ind;
+    }
+};
